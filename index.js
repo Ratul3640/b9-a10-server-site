b0c63bfd102424e113e0da1cbdd73cd9d261e4b4
Nahanur-islam <nahnanurism71@gmail.com>
2024-11-17 20:46:32 +0600

    username password added

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var cors = require('cors')
 const app = express()
-const port = 3000
+app.use(cors())
+app.use(express.json())
+const port = process.env.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9b6e9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const craftsCollection = client.db("assignment10DB").collection('crafts');
+
+
+        // work 1
+        app.post('/crafts', async (req, res) => {
+            const newCrafts = req.body;
+            console.log(newCrafts)
+            const result = await craftsCollection.insertOne(newCrafts);
+            res.send(result)
+        })
+
+        // work 2
+
+        app.get('/crafts', async (req, res) => {
+            const cursor = craftsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+
+        })
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
