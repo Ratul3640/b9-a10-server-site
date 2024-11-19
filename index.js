58405e37943a18e92bbdef58b68a9a4a246f9e3f
Nahanur-islam <nahnanurism71@gmail.com>
2024-11-19 20:36:40 +0600

    server site completed

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ app.use(cors())
 app.use(express.json())
 
 const port = process.env.PORT || 5000;
-
-
 // ZBN0ICl9Kf9USaeG
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9b6e9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri)
